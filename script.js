329d226f2950a32b48d54abe7619f9e4b8b4c52a
Matthew Courtney <matthewdc182@gmail.com>
2020-12-14 18:03:05 -0500

    got the ajax call up and running

--- script.js
+++ script.js
@@ -1,7 +1,30 @@
-$("#search").on("click", function() {
+$("#search").on("click", function () {
+    // prevents the page from restarting
     event.preventDefault();
+
     console.log("clicked");
-    console.log($("#input-value").val());
+
+    // settin search query to the input value
+    var searchQuery = $("#input-value").val().trim();
+    console.log(searchQuery);
+
+    // setting up variables for ajax call
+    var APIKey = "166a433c57516f51dfab1f7edaed8413";
+    var queryURL = "https://api.openweathermap.org/data/2.5/weather?" +
+        "q=" + searchQuery + "&appid=" + APIKey;
+
+        console.log(queryURL);
+        
+    // Ajax call 
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    })
+        // We store all of the retrieved data inside of an object called "response"
+        .then(function (response) {
+            console.log(response)
+        })
+
 
     $(".hidden").css("display", "inline-block");
 })
